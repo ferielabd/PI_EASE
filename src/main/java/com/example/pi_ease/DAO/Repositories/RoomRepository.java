4dfdd41d8b98ca591a1bedab5c52e62c56e0a2a8
feriel abdellatif <99718520+ferielabd@users.noreply.github.com>
2023-04-03 22:01:48 +0100

    Delete RoomRepository.java

--- src/main/java/com/example/pi_ease/DAO/Repositories/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pi_ease.DAO.Repositories;
-
-import com.example.pi_ease.DAO.Entities.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room,Integer> {
-}
