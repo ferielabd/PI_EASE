cd12ad93da9a38e66d364665cbc35e21338e5727
feriel abdellatif <99718520+ferielabd@users.noreply.github.com>
2023-04-05 06:54:59 +0100

    Delete src directory

--- src/main/java/com/example/pi_ease/DAO/Entities/TypeAccount.java
+++
@@ -1,5 +0,0 @@
-package com.example.pi_ease.DAO.Entities;
-
-public enum TypeAccount {
-    SAVING_ACCOUNT,CURRENT_ACCOUNT
-}
--- src/main/java/com/example/pi_ease/DAO/Entities/TypeRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.pi_ease.DAO.Entities;
-
-public enum TypeRole {
-
-    ADMIN,CLIENT,AGENT_SALES,INVESTOR_P,INVESTOR_NP;
-}
